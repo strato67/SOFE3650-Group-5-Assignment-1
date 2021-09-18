e8fdaeb55a91b45b51f6ae5073dbeb712301e08b
Saaransh Sharma <saaranshsharma28@gmail.com>
2021-09-18 18:23:20 -0400

    Created factory classes

---
+++ src/com/company/macFactory.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class macFactory implements productMaker{
+    @Override
+    public void createiPhone() {
+
+    }
+
+    @Override
+    public void createMac() {
+
+    }
+}
---
+++ src/com/company/phoneFactory.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class phoneFactory implements productMaker{
+    @Override
+    public void createiPhone() {
+
+    }
+
+    @Override
+    public void createMac() {
+
+    }
+}
